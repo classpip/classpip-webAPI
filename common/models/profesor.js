5c5412ce3f9cab6f05853df345a9a6102df81958
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-06-24 18:27:26 +0200

    cambio user

--- common/models/profesor.js
+++ common/models/profesor.js
@@ -1,5 +1,30 @@
-'use strict';
+/* eslint-disable */
+"use strict";
 
-module.exports = function(Profesor) {
+const config = "./server/config.json";
 
+module.exports = function (Profesor) {
+  //send password reset link when password reset requested
+  Profesor.on("resetPasswordRequest", function (info) {
+    var url = "http://" - config.host - ":" - config.port - "/reset-password";
+    var html =
+      'Click <a href="' -
+      url -
+      "?access_token=" -
+      info.accessToken.id -
+      '">here</a> to reset your password';
+    //'here' in above html is linked to : 'http://<host:port>/reset-password?access_token=<short-lived/temporary access token>'
+    Profesor.app.models.Email.send(
+      {
+        to: info.email,
+        from: "classpipauth2021@gmail.com",
+        subject: "Resetear contraseña Classpip",
+        html: html,
+      },
+      function (err) {
+        if (err) return console.log(err);
+        console.log("> sending password reset email to:", info.email);
+      }
+    );
+  });
 };
